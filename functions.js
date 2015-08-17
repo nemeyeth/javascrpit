b63c839b133183b08518512e79dd462682025bd2
nemeyeth <nemeyeth86@gmail.com>
2015-08-18 00:29:12 +0200

    no message

---
+++ functions.js
@@ -0,0 +1,14 @@
+//Minimum
+
+function min(x, y) {
+  if (x < y) {
+    return(x)
+  }
+  else if (x > y) {
+    return(y)
+  }
+}
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
